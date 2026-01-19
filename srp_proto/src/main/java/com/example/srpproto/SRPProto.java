a1a8bac65cdaef8015903f522ce9a5cfaa04f730
neiltheknight1844-collab <neil.the.knight1844@gmail.com>
2026-01-19 16:09:17 -0500

    Add ModSounds registration in SRPProto constructor

--- srp_proto/src/main/java/com/example/srpproto/SRPProto.java
+++ srp_proto/src/main/java/com/example/srpproto/SRPProto.java
@@ -2,6 +2,7 @@ package com.example.srpproto;
 
 import com.example.srpproto.registry.ModEntities;
 import com.example.srpproto.registry.ModItems;
+import com.example.srpproto.registry.ModSounds;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -14,6 +15,8 @@ public class SRPProto {
     public SRPProto() {
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        // ORDER IS IMPORTANT
+        ModSounds.register(modBus);
         ModEntities.register(modBus);
         ModItems.register(modBus);
     }
